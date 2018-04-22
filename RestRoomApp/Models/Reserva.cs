041d6250d59dfad6f470684836402fbbf2477069
David Blas Vázquez <david.bv@gmail.com>
2018-04-22 11:59:58 +0200

    Modelo con validaciones

--- RestRoomApp/Models/Reserva.cs
+++ RestRoomApp/Models/Reserva.cs
@@ -8,7 +8,11 @@ namespace RestRoomApp.Models
         public int ReservaID { get; set; }
         public int ClienteID { get; set; }
         public int HabitacionID { get; set; }
+        [Required(ErrorMessage = "Tiempo mínimo de alquier, 1h")]
+        [Range(1,8, ErrorMessage ="Tiempo maximo de alquier, 8h")]
         public int HorasReservacion { get; set; }
+        [Required(ErrorMessage = "Indica un hora de inicio")]
+        [Range(0, 23, ErrorMessage = "Rango entre la 00 horas y 23 horas")]
         public int HoraInicioReservacion { get; set; }
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         [DataType(DataType.Date)]
